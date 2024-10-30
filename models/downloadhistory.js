ff36bcd21175d0bce076928d5666cd49b2a142db
Nethravathibs123 <nethranethra451@gmail.com>
2024-10-31 03:02:58 +0530

    first commit

---
+++ models/downloadhistory.js
@@ -0,0 +1,19 @@
+
+const sequelize=require('../util/database')
+const Sequelize=require('sequelize')
+
+const downloadhistory=sequelize.define('downloadhistory',{
+id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+},
+link:{
+    type:Sequelize.STRING,
+    unique:true
+}
+
+})
+
+module.exports=downloadhistory;
